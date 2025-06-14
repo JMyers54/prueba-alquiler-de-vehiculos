dd6683e415d562c49d6eefdce84203e1ce24070e
JMyers54 <juandiegoquintero455@gmail.com>
2025-06-14 18:19:57 -0500

    se agrego la estructura de los metodos de cada clase

--- Controllers/funciones.py
+++ Controllers/funciones.py
@@ -13,11 +13,46 @@ class Administrador():
         self.Apellido = Apellido
         self.Telefono = Telefono
         self.Email = Email
+    
+    def gestionarEmpleados(self):
+        pass
+    
+    def gestionarCliente(self):
+        pass
+
+    def agregarVehiculos(self):
+        pass
+
+    def eliminarVehiculos(self):
+        pass
+
+    def informeUsoVehiculos(self):
+        pass
+
+
 class Empleado(Administrador):
     def __init__(self,Cedula,Nombre,Apellido,Telefono,Email):
         super().__init__(Cedula,Nombre,Apellido,Telefono,Email)
+    
+    def  gestionarClientes(self):
+        pass
+
+    def gestionarAlquiler(self):
+        pass
+
+    def gestionarDevolucion(self):
+        pass
 
 class Cliente(Empleado):
     def __init__(self,Cedula,Nombre,Apellido,Telefono,Email,LincenciaDeConducir):
         super().__init__(Cedula,Nombre,Apellido,Telefono,Email)
         self.LincenciaDeConduncir = LincenciaDeConducir
+    
+    def ConsultarVehiculosDisponibles(self):
+        pass
+
+    def RealizarReserva(Self):
+        pass
+
+    def DevolverVehiculo(self):
+        pass
