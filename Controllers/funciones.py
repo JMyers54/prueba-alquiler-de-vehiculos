23597d6e7c75c5395e64201023fc6a8292dc3494
JMyers54 <juandiegoquintero455@gmail.com>
2025-06-14 17:55:29 -0500

    se agregaron nuevas clases como Administrador,Cliente,Empleado

--- Controllers/funciones.py
+++ Controllers/funciones.py
@@ -6,3 +6,18 @@ class Funciones():
     def __init__(self, ventana):
         self.ventana = ventana
 
+class Administrador():
+    def __init__(self,Cedula,Nombre,Apellido,Telefono,Email):
+        self.Cedula = Cedula
+        self.Nombre = Nombre
+        self.Apellido = Apellido
+        self.Telefono = Telefono
+        self.Email = Email
+class Empleado(Administrador):
+    def __init__(self,Cedula,Nombre,Apellido,Telefono,Email):
+        super().__init__(Cedula,Nombre,Apellido,Telefono,Email)
+
+class Cliente(Empleado):
+    def __init__(self,Cedula,Nombre,Apellido,Telefono,Email,LincenciaDeConducir):
+        super().__init__(Cedula,Nombre,Apellido,Telefono,Email)
+        self.LincenciaDeConduncir = LincenciaDeConducir
