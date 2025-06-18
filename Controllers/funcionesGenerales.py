65e4e6276c90ddf1fe898cc66fbdef96526ae878
JMyers54 <juandiegoquintero455@gmail.com>
2025-06-17 23:12:04 -0500

    se agrego función para q el administrador inice sesion

--- Controllers/funcionesGenerales.py
+++ Controllers/funcionesGenerales.py
@@ -6,7 +6,7 @@ from Models.ConexionBD import ConexionDB
 class Funciones():
     def __init__(self, ventana):
         self.ventana = ventana
-    pass
+    
 
     def registrar_empleado(self, cedula, nombre, apellido, telefono, email, contra):
         try:
@@ -14,15 +14,15 @@ class Funciones():
             conexion.crearConexion()
             db = conexion.getConnection()
             cursor = db.cursor()
-            sql = "INSERT INTO empleados (Cedula, Nombre, Apellido, Telefono, Contraseña, Email) VALUES (%s, %s, %s, %s, %s, %s)"
+            sql = "INSERT INTO empleados (Cedula, Nombre, Apellido, Telefono, Email, Contraseña) VALUES (%s, %s, %s, %s, %s, %s)"
             datos = (cedula, nombre, apellido, telefono, email, contra)
             cursor.execute(sql, datos)
             db.commit()
             cursor.close()
             conexion.cerrarConexion()
-            messagebox.showinfo("empleado registrado con éxito")
+            messagebox.showinfo(print("exito","empleado registrado con éxito"))
         except Exception as e:
-            messagebox.showerror("Error",f"al registrar empleado: {e}")
+            messagebox.showerror(print("Error",f"al registrar empleado: {e}"))
 
     def solicitarClave(self):
         clave = simpledialog.askstring("clave de Administrador", "Ingrese la clave de Administrador: ")
@@ -46,6 +46,32 @@ class Funciones():
 
             if resultado is None:
                 print("Cédula no registrada.")
+                return False
+
+            if resultado[0] == contra:
+                return True
+            else:
+                print("Contraseña incorrecta.")
+                return False
+
+        except Exception as e:
+            print(f"Error al iniciar sesión: {e}")
+            return False
+
+    def iniciar_sesion_admin(self, id, contra):
+        try:
+            conexion = ConexionDB()
+            conexion.crearConexion()
+            db = conexion.getConnection()
+
+            with db.cursor() as cursor:
+                cursor.execute("SELECT Contraseña FROM administradores WHERE id = %s", (id,))
+                resultado = cursor.fetchone()
+
+            conexion.cerrarConexion()
+
+            if resultado is None:
+                print("id no registrado.")
                 return False
 
             if resultado[0] == contra:
